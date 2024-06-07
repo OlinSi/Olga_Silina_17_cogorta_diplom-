cfe852ad5cb33d5037754a011025369f02ddc552
OlinSi <olenenok-pinky@mail.ru>
2024-06-07 15:24:13 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Ольга Силина, 17-я когорта — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://0f5d121a-38e8-4d52-8415-d454670f274e.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
